353290e9b7e1e64583c2b11226712f1039775341
Márcio Rosa <protonss@ig.com.br>
2013-03-25 13:21:42 -0300

    Correcao do bug de reserva

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -30,4 +30,15 @@ public class SessaoTest {
 		sessao.reserva(3);
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
 	}
+	
+	@Test
+	public void deveReservarSessaoComNumeroDisponivelIgualSolicitado() throws Exception {
+		
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(1);
+		Assert.assertTrue(sessao.podeReservar(1));
+		
+	}
+	
+	
 }
